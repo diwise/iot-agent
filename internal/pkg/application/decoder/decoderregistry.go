0e1707dcde270d9fad1ca2d6d620747682f25f00
shestyf <sofia.styf@masarinconsulting.se>
2022-12-08 08:52:08 +0100

    replaced space with underline

--- internal/pkg/application/decoder/decoderregistry.go
+++ internal/pkg/application/decoder/decoderregistry.go
@@ -23,7 +23,7 @@ func NewDecoderRegistry() DecoderRegistry {
 		"tem_lab_14ns":     SenlabTBasicDecoder,
 		"strips_lora_ms_h": SensativeDecoder,
 		"cube02":           SensefarmBasicDecoder,
-		"milesight am100":  MilesightDecoder,
+		"milesight_am100":  MilesightDecoder,
 	}
 
 	return &decoderRegistry{
