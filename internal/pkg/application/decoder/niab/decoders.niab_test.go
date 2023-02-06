ffec60f0e6bcead5619c9d6cc6bad4b832eb49f0
Isak Styf <isak.styf@masarinconsulting.se>
2023-02-06 16:07:50 +0100

    add decoder for niab fill level sensor

---
+++ internal/pkg/application/decoder/niab/decoders.niab_test.go
@@ -0,0 +1,93 @@
+package niab
+
+import (
+	"context"
+	"testing"
+
+	"github.com/diwise/iot-agent/internal/pkg/application"
+	"github.com/diwise/iot-agent/internal/pkg/application/decoder/payload"
+	"github.com/matryer/is"
+)
+
+func TestNIAB(t *testing.T) {
+	is := is.New(t)
+
+	evt := sensorEventFromTestData([]byte{0xcc, 0x0f, 0x03, 0xc5})
+	callBackCount := 0
+
+	FillLevelSensorDecoder(context.Background(), evt, func(ctx context.Context, p payload.Payload) error {
+		battery, ok := payload.Get[int](p, "batteryLevel")
+		is.True(ok)
+		is.Equal(battery, 80)
+
+		temperature, ok := payload.Get[float64](p, "temperature")
+		is.True(ok)
+		is.Equal(temperature, 15.0)
+
+		distance, ok := payload.Get[int](p, "distance")
+		is.True(ok)
+		is.Equal(distance, 965)
+
+		callBackCount = 1
+		return nil
+	})
+
+	// Make sure that the callback was in fact invoked
+	is.Equal(callBackCount, 1)
+}
+
+func TestNIABIgnoresReadErrors(t *testing.T) {
+	is := is.New(t)
+
+	evt := sensorEventFromTestData([]byte{0xcc, 0x0f, 0xff, 0xff})
+	callBackCount := 1
+
+	FillLevelSensorDecoder(context.Background(), evt, func(ctx context.Context, p payload.Payload) error {
+		_, ok := payload.Get[int](p, "distance")
+		is.True(!ok)
+
+		callBackCount = 1
+		return nil
+	})
+
+	// Make sure that the callback was in fact invoked
+	is.Equal(callBackCount, 1)
+}
+
+func TestNIABIgnoresTruncatedData(t *testing.T) {
+	is := is.New(t)
+
+	evt := sensorEventFromTestData([]byte{0xcc, 0x0f})
+	callBackCount := 0
+
+	err := FillLevelSensorDecoder(context.Background(), evt, func(ctx context.Context, p payload.Payload) error {
+		callBackCount = 1
+		return nil
+	})
+
+	// Make sure that the callback was not invoked
+	is.Equal(callBackCount, 0)
+	is.True(err != nil)
+}
+
+func TestNIABIgnoresTooLongData(t *testing.T) {
+	is := is.New(t)
+
+	evt := sensorEventFromTestData([]byte{0xcc, 0x0f, 0xff, 0xff, 0xff, 0xff})
+	callBackCount := 0
+
+	err := FillLevelSensorDecoder(context.Background(), evt, func(ctx context.Context, p payload.Payload) error {
+		callBackCount = 1
+		return nil
+	})
+
+	// Make sure that the callback was not invoked
+	is.Equal(callBackCount, 0)
+	is.True(err != nil)
+}
+
+func sensorEventFromTestData(data []byte) application.SensorEvent {
+	return application.SensorEvent{
+		Data: data,
+	}
+}
