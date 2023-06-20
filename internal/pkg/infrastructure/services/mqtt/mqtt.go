ca9110fea0a7720cd6767f8e3fc2b2b973baf6e2
Isak Styf <isak.styf@masarinconsulting.se>
2023-06-20 10:52:22 +0200

    add support for custom MQTT keep alive value

--- internal/pkg/infrastructure/services/mqtt/mqtt.go
+++ internal/pkg/infrastructure/services/mqtt/mqtt.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"os"
+	"strconv"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/google/uuid"
@@ -16,11 +17,12 @@ type Client interface {
 }
 
 type Config struct {
-	enabled  bool
-	host     string
-	user     string
-	password string
-	topics   []string
+	enabled   bool
+	host      string
+	keepAlive int64
+	user      string
+	password  string
+	topics    []string
 }
 
 func NewClient(logger zerolog.Logger, cfg Config, forwardingEndpoint string) (Client, error) {
@@ -66,10 +68,11 @@ func NewConfigFromEnvironment(prefix string) (Config, error) {
 	const topicEnvNamePattern string = "%sMQTT_TOPIC_%d"
 
 	cfg := Config{
-		enabled:  os.Getenv(fmt.Sprintf("%sMQTT_DISABLED", prefix)) != "true",
-		host:     os.Getenv(fmt.Sprintf("%sMQTT_HOST", prefix)),
-		user:     os.Getenv(fmt.Sprintf("%sMQTT_USER", prefix)),
-		password: os.Getenv(fmt.Sprintf("%sMQTT_PASSWORD", prefix)),
+		enabled:   os.Getenv(fmt.Sprintf("%sMQTT_DISABLED", prefix)) != "true",
+		host:      os.Getenv(fmt.Sprintf("%sMQTT_HOST", prefix)),
+		keepAlive: 30,
+		user:      os.Getenv(fmt.Sprintf("%sMQTT_USER", prefix)),
+		password:  os.Getenv(fmt.Sprintf("%sMQTT_PASSWORD", prefix)),
 		topics: []string{
 			os.Getenv(fmt.Sprintf(topicEnvNamePattern, prefix, 0)),
 		},
@@ -85,6 +88,15 @@ func NewConfigFromEnvironment(prefix string) (Config, error) {
 
 	if cfg.topics[0] == "" {
 		return cfg, fmt.Errorf("at least one topic (%sMQTT_TOPIC_0) must be added to the configuration", prefix)
+	}
+
+	customKeepAlive := os.Getenv(fmt.Sprintf("%sMQTT_KEEPALIVE", prefix))
+	if customKeepAlive != "" {
+		keepAlive, err := strconv.ParseInt(customKeepAlive, 10, 64)
+		if err != nil {
+			return cfg, fmt.Errorf("custom keepalive value %s is not parseable to an int (%s)", customKeepAlive, err.Error())
+		}
+		cfg.keepAlive = keepAlive
 	}
 
 	const maxTopicCount int = 10
