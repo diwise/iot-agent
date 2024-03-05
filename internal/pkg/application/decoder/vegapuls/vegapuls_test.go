dedeca4d8d309ce6403791b704d875bdb392b0c7
shestyf <sofia.styf@masarinconsulting.se>
2024-03-05 15:23:12 +0100

    added two small unit tests

--- internal/pkg/application/decoder/vegapuls/vegapuls_test.go
+++ internal/pkg/application/decoder/vegapuls/vegapuls_test.go
@@ -27,6 +27,24 @@ func TestVegapulsSensor(t *testing.T) {
 	is.Equal(tmp.SensorValue, float64(25))
 }
 
+func TestVegapulsSensorReturnsErrOnIncompletePayload(t *testing.T) {
+	is, _ := testSetup(t)
+	ue, err := application.Netmore([]byte(partialTestData))
+	is.NoErr(err)
+
+	_, err = Decoder(context.Background(), "devid", ue)
+	is.True(err != nil)
+}
+
+func TestVegapulsSensorReturnsErrOnUnknownPacketIndicator(t *testing.T) {
+	is, _ := testSetup(t)
+	ue, err := application.Netmore([]byte(unknownTestData))
+	is.NoErr(err)
+
+	_, err = Decoder(context.Background(), "devid", ue)
+	is.True(err != nil)
+}
+
 func testSetup(t *testing.T) (*is.I, *slog.Logger) {
 	is := is.New(t)
 	return is, slog.New(slog.NewTextHandler(io.Discard, nil))
@@ -38,6 +56,36 @@ const testData string = `[{
 	"sensorType":"vegapuls_air_41",
 	"fPort":"1",
 	"payload":"02003FA31F152D2400FA09",
+	"timestamp":"2024-02-28T11:21:59.626943Z",
+	"rxInfo":{
+		"gatewayId":"274",
+		"rssi":-107,"snr":4
+	},
+	"txInfo":{},
+	"error":{}
+}]`
+
+const partialTestData string = `[{
+	"devEui":"c4ac590000ccc60d",
+	"deviceName":"vegapuls_air_41",
+	"sensorType":"vegapuls_air_41",
+	"fPort":"1",
+	"payload":"02003FA31F152D2400FA",
+	"timestamp":"2024-02-28T11:21:59.626943Z",
+	"rxInfo":{
+		"gatewayId":"274",
+		"rssi":-107,"snr":4
+	},
+	"txInfo":{},
+	"error":{}
+}]`
+
+const unknownTestData string = `[{
+	"devEui":"c4ac590000ccc60d",
+	"deviceName":"vegapuls_air_41",
+	"sensorType":"vegapuls_air_41",
+	"fPort":"1",
+	"payload":"05003FA31F152D2400FA",
 	"timestamp":"2024-02-28T11:21:59.626943Z",
 	"rxInfo":{
 		"gatewayId":"274",
