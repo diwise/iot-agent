f32deace076d0f91ceb7f2eb63089ab5481c107a
Mikael Nordberg <mikael.nordberg@masarinconsulting.se>
2024-09-18 15:19:59 +0200

    mod up + tests

--- internal/pkg/application/decoder/elsys/decoders.elsys_test.go
+++ internal/pkg/application/decoder/elsys/decoders.elsys_test.go
@@ -128,10 +128,37 @@ func TestDecodeElsysPayload(t *testing.T) {
 	is.Equal(string(j), `[{"bn":"devId/3303/","bt":1698674257,"n":"0","vs":"urn:oma:lwm2m:ext:3303"},{"n":"5700","u":"Cel","v":7.5},{"bn":"devId/3304/","bt":1698674257,"n":"0","vs":"urn:oma:lwm2m:ext:3304"},{"n":"5700","u":"%RH","v":84},{"bn":"devId/3/","bt":1698674257,"n":"0","vs":"urn:oma:lwm2m:ext:3"},{"n":"7","u":"mV","v":3642},{"bn":"devId/3200/","bt":1698674257,"n":"0","vs":"urn:oma:lwm2m:ext:3200"},{"n":"5500","vb":false}]`)
 }
 
+func TestElsysElt2hpTrue(t *testing.T) {
+	is, _ := testSetup(t)
+
+	ue, _ := application.Netmore([]byte(elt2hp_true))
+	objects, err := Decoder(context.Background(), "devId", ue)
+	is.NoErr(err)
+
+	is.Equal(len(objects), 4)
+	is.True(objects[3].(lwm2m.DigitalInput).DigitalInputState)
+}
+
 func testSetup(t *testing.T) (*is.I, *slog.Logger) {
 	is := is.New(t)
 	return is, slog.New(slog.NewTextHandler(io.Discard, nil))
 }
+
+const elt2hp_true string = `[{
+	"devEui":"a81758fffe09ec03",
+	"deviceName":"elt_2_hp",
+	"sensorType":"elt_2_hp",
+	"fPort":"5",
+	"payload":"010096024e070e1e0d0114000fdcc01a00",
+	"timestamp":"2023-10-30T13:57:37.868543Z",
+	"rxInfo":{
+		"gatewayId":"881",
+		"rssi":-117,
+		"snr":-17
+	},
+	"txInfo":{},
+	"error":{}
+}]`
 
 const elt2hp string = `[{
 	"devEui":"a81758fffe09ec03",
