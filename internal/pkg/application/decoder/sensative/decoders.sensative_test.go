d727e51a0587d487b73ac27ad0035407411999bc
Mikael Nordberg <mikael.nordberg@masarinconsulting.se>
2024-11-12 10:30:10 +0100

    diffrent checkins

--- internal/pkg/application/decoder/sensative/decoders.sensative_test.go
+++ internal/pkg/application/decoder/sensative/decoders.sensative_test.go
@@ -3,11 +3,13 @@ package sensative
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log/slog"
 	"testing"
 
 	"github.com/diwise/iot-agent/internal/pkg/application"
+	"github.com/diwise/iot-agent/pkg/lwm2m"
 
 	"github.com/matryer/is"
 )
@@ -19,6 +21,7 @@ func TestPresenceSensorReading(t *testing.T) {
 	objects, err := Decoder(context.Background(), "devID", ue)
 	is.NoErr(err)
 	is.Equal(objects[0].ID(), "devID")
+	is.Equal(objects[0].(lwm2m.Presence).DigitalInputState, true)
 }
 
 func TestPresenceSensorPeriodicCheckIn(t *testing.T) {
@@ -30,6 +33,26 @@ func TestPresenceSensorPeriodicCheckIn(t *testing.T) {
 	objects, err := Decoder(context.Background(), "devID", ue)
 	is.NoErr(err)
 	is.Equal(objects[0].ID(), "devID")
+}
+
+func TestDataErrSensorReading(t *testing.T) {
+	is, _ := testSetup(t)
+	ue := application.SensorEvent{}
+
+	payload := []string{
+		"//9uAxL8UAAAAAA=",
+		"//9uAxL8UAUAAAA=",
+		"//9uAxL8UPkAAAA=",
+		"//9uAxL8UPwAAAA=",
+	}
+
+	for _, p := range payload {
+		d := fmt.Sprintf(checkin, p)
+		err := json.Unmarshal([]byte(d), &ue)
+		is.NoErr(err)
+		_, err = Decoder(context.Background(), "devID", ue)
+		is.NoErr(err)
+	}
 }
 
 func testSetup(t *testing.T) (*is.I, *slog.Logger) {
@@ -61,3 +84,29 @@ const livboj string = `
 }`
 
 const livboj_checkin string = `{"devEui":"3489573498573459","deviceName":"Livboj","sensorType":"Sensative_Codec","fPort":1,"data":"//9uAxL8UAAAAAA=","object":{"buildId":{"id":51575888,"modified":false},"historySeqNr":65535,"prevHistSeqNr":65535},"timestamp":"2022-11-04T06:42:44.274490703Z","rxInfo":{"gatewayId":"fcc23dfffe2ee936","uplinkId":"23bab2ad-f4d0-4175-b09e-d1177dea44e0","rssi":-111,"snr":-8},"txInfo":{}}`
+
+const checkin string = `
+{
+  "data": "%s",
+  "error": {},
+  "fPort": 1,
+  "devEui": "a4bc",
+  "object": {
+    "buildId": {
+      "id": 51575888,
+      "modified": false
+    },
+    "historySeqNr": 65535,
+    "prevHistSeqNr": 65535
+  },
+  "rxInfo": {
+    "snr": -14,
+    "rssi": -127,
+    "uplinkId": "00a2332a-cead-4974-a92f-8ee199747b1a",
+    "gatewayId": "0016c001ff10c7f6"
+  },
+  "txInfo": {},
+  "timestamp": "2024-11-12T07:03:37.187954862Z",
+  "deviceName": "Livboj-05",
+  "sensorType": "Sensative_Codec"
+}`
